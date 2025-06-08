f1dbbed9a473223ef9bcdcb01ab0cdf524bfd514
RyuuXiao <164801435+RyuuXiaoo@users.noreply.github.com>
2025-06-08 10:49:40 +0700

    Update bdsm.js

--- src/api/nsfw/bdsm.js
+++ src/api/nsfw/bdsm.js
@@ -1,19 +1,21 @@
 const axios = require('axios');
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get('/nsfw/bdsm', async (req, res) => {
     try {
       const response = await axios.get('https://api.nekorinn.my.id/nsfwhub/bdsm');
-      res.status(200).json({
+      const data = response.data;
+
+      res.json({
         status: true,
-        creator: "RyuuXiao",
-        result: response.data.result
+        creator: 'RyuuXiao',
+        result: data.result // <-- ini WAJIB agar gambar bisa ditampilkan di UI
       });
     } catch (error) {
       res.status(500).json({
         status: false,
-        creator: "RyuuXiao",
-        message: error.message
+        message: 'Gagal mengambil data',
+        error: error.message
       });
     }
   });
