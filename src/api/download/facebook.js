a13e39a6d0de0508c5791d7f9b45e5befcf916cd
RyuuXiao <164801435+RyuuXiaoo@users.noreply.github.com>
2025-06-27 23:26:16 +0700

    Add files via upload

--- src/api/download/facebook.js
+++ src/api/download/facebook.js
@@ -1,53 +1,16 @@
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const api = require("@fongsidev/scraper")
 
-module.exports = function(app) {
-
-function generatePayload(url) {
-  return {
-    id: url,
-    locale: 'id'
-  };
-}
-
-async function fbdl(url) {
-  const payload = generatePayload(url);
-  const { data } = await axios.post('https://getmyfb.com/process', payload);
-  const $ = cheerio.load(data);
-
-  const downloadLinks = [];
-
-  $('.results-list-item').each((index, element) => {
-    const quality = $(element).text().replace(/\s+/g, '');
-    const link = $(element).find('a').attr('href');
-    const filename = $(element).find('a').attr('download');
-
-    if (link) {
-      if (filename) {
-        downloadLinks.push({ quality, link, filename });
-      } else {
-        downloadLinks.push({ quality, link });
-      }
-    }
-  });
-
-  return downloadLinks;
-}
-
-// REST API Endpoint
+module.exports = function (app) {
 app.get('/download/facebook', async (req, res) => {
-  const { url } = req.query;
-  if (!url) return res.status(400).json({ error: 'URL Facebook tidak boleh kosong' });
-
-  try {
-    const links = await fbdl(url);
-    if (!links.length) {
-      return res.status(404).json({ error: 'Tidak ditemukan link download' });
-    }
-    res.json({ success: true, results: links });
-  } catch (error) {
-    res.status(500).json({ error: 'Gagal mengambil data', detail: error.message });
-  }
+       const { url } = req.query
+        try {
+            const results = await api.Facebook(url);
+            res.status(200).json({
+                status: true,
+                result: results.data
+            });
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
 });
 }
