87bdb2a97750d1690df7a4cdf18d4031309ffef5
Ryuu311 <aldrian.gaming.anjay@gmail.com>
2025-08-04 23:23:03 +0700

    Add files via upload

--- src/api/download/youtube.js
+++ src/api/download/youtube.js
@@ -1,55 +1,69 @@
 const express = require('express');
 const axios = require('axios');
+const vm = require('node:vm');
+const app = express();
 
-module.exports = function(app) {
-    async function reinzmahiruytdl(link) {
-        try {
-            const response = await axios.get('https://y2ts.us.kg/token');
-            const token = response.data.token;
+async function XeonInstaMp4(link) {
+    let body = new URLSearchParams({
+        "sf_url": encodeURI(link),
+        "sf_submit": "",
+        "new": 2,
+        "lang": "id",
+        "app": "",
+        "country": "id",
+        "os": "Windows",
+        "browser": "Chrome",
+        "channel": " main",
+        "sf-nomad": 1
+    });
 
-            const url = `https://y2ts.us.kg/youtube?url=${link}`;
-            const headers = {
-                'Authorization-Token': token,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-                'Content-Type': 'application/json'
-            };
-
-            const videoResponse = await axios.get(url, { headers });
-            if (videoResponse.data.status) {
-                return videoResponse.data.result;
-            } else {
-                throw new Error('Gagal mengambil video, status = false');
-            }
-
-        } catch (error) {
-            throw new Error(error.message || 'Gagal memproses permintaan');
-        }
-    }
-
-    app.get('/download/youtube', async (req, res) => {
-        const { url } = req.query;
-
-        if (!url) {
-            return res.status(400).json({
-                success: false,
-                message: 'Parameter "url" is required'
-            });
-        }
-
-        try {
-            const hasil = await reinzmahiruytdl(url);
-            res.json({
-                status: true,
-                creator: "RyuuDev",
-                result: hasil
-            });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({
-                statu: false,
-                creator: "RyuuDev",
-                message: error.message
-            });
+    let { data } = await axios({
+        url: "https://worker.sf-tools.com/savefrom.php",
+        method: "POST",
+        data: body,
+        headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            "origin": "https://id.savefrom.net",
+            "referer": "https://id.savefrom.net/",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
         }
     });
-};
+
+    let exec = '[]["filter"]["constructor"](b).call(a);';
+    data = data.replace(exec, `\ntry {\ni++;\nif (i === 2) scriptResult = ${exec.split(".call")[0]}.toString();\nelse (\n${exec.replace(/;/, "")}\n);\n} catch {}`);
+    let context = {
+        "scriptResult": "",
+        "i": 0
+    };
+    vm.createContext(context);
+    new vm.Script(data).runInContext(context);
+
+    return JSON.parse(context.scriptResult.split("window.parent.sf.videoResult.show(")?.[1].split(");")?.[0]);
+}
+
+app.get('/download/youtube', async (req, res) => {
+    const { url } = req.query;
+
+    if (!url) {
+        return res.status(400).json({
+            success: false,
+            message: 'Parameter "url" is required'
+        });
+    }
+
+    try {
+        const result = await XeonInstaMp4(url);
+        res.json({
+            success: true,
+            creator "RyuuDev",
+            output: result
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            creator "RyuuDev",
+            message: err.message || 'Internal Server Error'
+        });
+    }
+});
