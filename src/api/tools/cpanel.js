1a5e9b52f9303c7c88bdb84dad5f8659808f1382
Ryuu311 <aldrian.gaming.anjay@gmail.com>
2025-08-15 19:54:25 +0700

    Add files via upload

--- src/api/tools/cpanel.js
+++ src/api/tools/cpanel.js
@@ -45,7 +45,7 @@ module.exports = function(app) {
           username,
           first_name: username,
           last_name: username,
-          password: password
+          password: password,
           language: "en",
           password
         })
