e06e50417b336826d938731c4eed7ddaab7f4bc8
Ryuu311 <aldrian.gaming.anjay@gmail.com>
2025-08-18 14:59:00 +0700

    Add files via upload

--- src/api/tools/smeme.js
+++ src/api/tools/smeme.js
@@ -92,7 +92,7 @@ module.exports = function (app) {
           ctx.strokeText(line, size / 2, lineY);
 
           // Outline tipis
-          ctx.lineWidth = Math.floor(fontSize / 1);
+          ctx.lineWidth = Math.floor(fontSize / 15);
           ctx.strokeText(line, size / 2, lineY);
 
           // Fill text
