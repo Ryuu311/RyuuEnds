eb18a115e7029546a0d03b0c5639e719e08271b5
Ryuu311 <aldrian.gaming.anjay@gmail.com>
2025-09-06 06:19:06 +0700

    Add files via upload

--- src/api/download/yt-dlp.js
+++ src/api/download/yt-dlp.js
@@ -1,16 +1,18 @@
 const { Client } = require('ssh2');  
-  
+const path = require('path');
+const fs = require('fs');
+
 async function downloadYouTube({ ip, port, username, password, url }) {  
   return new Promise((resolve, reject) => {  
     const conn = new Client();  
     let output = '';  
-  
+
     conn.on('ready', () => {  
-      const cmd = ` node ytdl-api/yt-dlp.js "${url}"`;  
-  
+      const cmd = `node ytdl-api/yt-dlp.js "${url}"`;  
+
       conn.exec(cmd, (err, stream) => {  
         if (err) return reject({ success: false, error: err.message });  
-  
+
         stream.on('close', (code, signal) => {  
           conn.end();  
           resolve({ success: true, code, output });  
@@ -30,13 +32,25 @@ async function downloadYouTube({ ip, port, username, password, url }) {
     });  
   });  
 }  
-  
+
+// Ambil apikey dari root folder
+const settingsPath = path.join(process.cwd(), 'src/settings.json');
+const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf-8'));
+global.apikey = Array.isArray(settings.apiSettings.apikey) ? settings.apiSettings.apikey : [];
+
 module.exports = function(app) {  
   app.get("/download/yt-dlp", async (req, res) => {  
     try {  
-      const { url } = req.query;  
+      const { url, apikey } = req.query;  
+
+      // Cek URL
       if (!url) return res.status(400).json({ status: false, error: "Masukkan URL YouTube" });  
-  
+
+      // Cek apikey
+      if (!apikey || !global.apikey.includes(apikey)) {
+        return res.status(403).json({ status: false, error: "Apikey kosong atau salah!" });
+      }
+
       const result = await downloadYouTube({  
         ip: '193.143.69.100',  
         port: 35678,  
@@ -44,11 +58,11 @@ module.exports = function(app) {
         password: 'ryuu',  
         url  
       });  
-  
+
       const output = typeof result.output === "string"  
         ? JSON.parse(result.output)  
         : result.output;  
-  
+
       res.json({  
         status: true,  
         creator: "RyuuDev",  
