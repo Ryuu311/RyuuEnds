e96a107206b5d4496e7b9c08a1a9d892b0679807
Ryuu311 <aldrian.gaming.anjay@gmail.com>
2025-09-18 13:52:51 +0700

    Add files via upload

--- src/api/download/tiktok.js
+++ src/api/download/tiktok.js
@@ -1,46 +1,19 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 module.exports = (app) => {
     const creatorName = "RyuuDev";
-
-  async function tiktokDl(url) {
+    
+async function tiktokDl(query) {
   try {
-    const response = await axios.post('https://api.ttsave.app/', {
-      id: url,
-      hash: 'eabd36f82466974a4527e6b997da38bf',
-      mode: 'video',
-      locale: 'id',
-      loading_indicator_url: 'https://ttsave.app/images/slow-down.gif',
-      unlock_url: 'https://ttsave.app/id/unlock'
+    const response = await axios.post("https://ttsave.app/download", {
+      query,
+      language_id: "2",
     });
 
-    const htmlContent = response.data;
-    const $ = cheerio.load(htmlContent);
-
-    const profileImage = $('img.h-24').attr('src');
-    const username = $('a[title]').text();
-    const description = $('p.text-gray-600').text();
-    const likeCount = $('span.text-gray-500').eq(0).text();
-    const commentCount = $('span.text-gray-500').eq(1).text();
-    const shareCount = $('span.text-gray-500').eq(2).text();
-    const downloadLinks = {
-      withoutWatermark: $('a[type="no-watermark"]').attr('href'),
-      withWatermark: $('a[type="watermark"]').attr('href')
-    };
-
-    return {
-      profileImage,
-      username,
-      description,
-      likeCount,
-      commentCount,
-      shareCount,
-      downloadLinks
-    };
+    return response.data;
   } catch (error) {
-    console.error('Error:', error.message);
-    throw error;
+    console.error("Error:", error.message);
+    return null;
   }
 }
 
